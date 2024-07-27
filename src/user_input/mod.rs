6d36310fbd705382c273a3fbac6d6c8022d2c129
Joseph Handsome <nottellingu@gmail.com>
2024-07-27 15:15:39 -0400

    new function with tests

--- src/user_input/mod.rs
+++ src/user_input/mod.rs
@@ -1,5 +1,7 @@
 mod get_input;
 
+use std::{collections::HashMap, usize};
+
 use get_input::get_input;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -49,6 +51,24 @@ pub struct UserInput {
 }
 
 impl UserInput {
+    // how I feel when understanding lifetimes: https://en.wikipedia.org/wiki/God
+    pub fn break_down_binary_address<'a>(&self, address: &'a String) -> (&'a str, &'a str, &'a str) {
+        let number_of_tag_bits = self.tag_size() as usize;
+        let number_of_set_bits = self.num_sets_exp() as usize;
+        let number_of_offset_bits = self.line_size_exp as usize;
+        let total_size = number_of_set_bits + number_of_tag_bits + number_of_offset_bits;
+        
+        // little sanity check
+        assert_eq!(32, address.len());
+        assert_eq!(32, total_size);
+
+        let tag = &address[..number_of_tag_bits];
+        let set = &address[number_of_tag_bits..number_of_tag_bits+number_of_set_bits];
+        let offset = &address[number_of_tag_bits+number_of_set_bits..];
+
+        return (tag, set, offset);
+    }
+
     fn new(cache_size_exp: u32, line_size_exp: u32, cache_type: String, replacement_policy: String) -> Self {
         let cache_type: CacheType = match cache_type.to_lowercase().trim() {
             "fa" => CacheType::FullyAssociative(cache_size_exp - line_size_exp),
@@ -71,14 +91,16 @@ impl UserInput {
         UserInput { cache_size_exp, line_size_exp, cache_type, replacement_policy: replacement_policy.into() }
     }
 
-    pub fn create_cache(&self){todo!()}
-
     fn num_lines_exp(&self) -> u32 {
         self.cache_size_exp - self.line_size_exp
     }
 
     fn num_sets_exp(&self) -> u32 {
         self.num_lines_exp() - self.cache_type.set_size_exp()
+    }
+
+    fn num_sets(&self) -> u32 {
+        self.num_sets_exp().pow(2)
     }
 
     fn tag_size(&self) -> u32 {
@@ -131,4 +153,31 @@ pub fn all_user_input() -> UserInput{
     });
 
     UserInput::new(cache_size_exp.parse().unwrap(), line_size_exp.parse().unwrap(), cache_type, replacement_policy)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decompose_binary_address_with_fa() {
+        let test_input = UserInput::new(5, 2, "fa".into(), "l".into());
+        // 0x1fffff50
+        let binary_address = "00011111111111111111111101010000".to_string();
+        let (tag, set, offset) = test_input.break_down_binary_address(&binary_address);
+        assert_eq!(tag, "000111111111111111111111010100");
+        assert_eq!(set, "");
+        assert_eq!(offset, "00");
+    }
+
+    #[test]
+    fn test_decompose_binary_address_with_dm() {
+        let test_input = UserInput::new(5, 2, "dm".into(), "l".into());
+        // 0x1fffff50
+        let binary_address = "00011111111111111111111101010000".to_string();
+        let (tag, set, offset) = test_input.break_down_binary_address(&binary_address);
+        assert_eq!(tag, "000111111111111111111111010");
+        assert_eq!(set, "100");
+        assert_eq!(offset, "00");
+    }
 }
