9d2357c01addf6d6d8a917577dc364f1b48b3680
Joseph Handsome <nottellingu@gmail.com>
2024-07-27 21:39:21 -0400

    done?

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,6 +8,8 @@ fn main() {
     let mut c = Cache::new(&ui);
     let sim_results = c.simulate_trace_file("trace_files/gcc.trace");
 
+    println!("hits: {}, accesses: {}, hit rate: {}", sim_results.hits, sim_results.accesses, sim_results.hits as f64 / sim_results.accesses as f64);
+
     let mut plot = Plot::new();
     let trace = Scatter::new(sim_results.accesses_history, sim_results.hit_history);
     plot.add_trace(trace);
